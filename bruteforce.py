099c7ae42a5f01a1e508d0066f314723d1e40289
Arnaud Lienard <lienard.a@me.com>
2024-02-02 09:54:24 +0100

    clean csv

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,17 @@
 import csv
 import time
 
-strart_time = time.time()  # Enregistrez le temps de début
-# Charger les données depuis le CSV
-with open("data/data_bruteforce.csv", "r", newline="", encoding="utf-8") as file:
-    csv_reader = csv.reader(file, delimiter=";")
-    headers = next(csv_reader)  # Récupérer les en-têtes
-    headers = [header.strip() for header in headers]  # Supprimer les espaces autour des noms de colonnes
-    headers[0] = headers[0].lstrip("\ufeff")  # Supprimer le caractère spécial '\ufeff' devant "name"
 
+def calculate_total_profit(combination):
+    return sum(float(action["profit"]) * int(action["price"]) / 100 for action in combination)
+
+
+start_time = time.time()  # Enregistrez le temps de début
+
+# Charger les données depuis le CSV
+with open("data/data_bruteforce.csv", "r", newline="", encoding="utf-8-sig") as file:
+    csv_reader = csv.reader(file, delimiter=",")
+    headers = next(csv_reader)  # Récupérer les en-têtes
     data_lines = list(csv_reader)
 
 # Convertir les données en une liste de dictionnaires
@@ -27,13 +30,11 @@ for i in range(2**num_actions):
     binary_representation = bin(i)[2:].zfill(num_actions)
     combination = [data[j] for j in range(num_actions) if binary_representation[j] == "1"]
 
-    current_price = sum(int(combination[j]["price"]) for j in range(len(combination)))
+    current_price = sum(int(action["price"]) for action in combination)
 
     # Vérifier si la combinaison respecte la contrainte de prix
     if current_price <= 500:
-        total_profit = sum(
-            float(combination[j]["profit"]) * int(combination[j]["price"]) / 100 for j in range(len(combination))
-        )
+        total_profit = calculate_total_profit(combination)
 
         # Mettre à jour la solution optimale si le profit est plus élevé
         if total_profit > max_profit:
@@ -43,7 +44,7 @@ for i in range(2**num_actions):
             total_price = current_price  # Mettre à jour le prix total
 
 end_time = time.time()  # Enregistrez le temps de fin
-execution_time = end_time - strart_time  # Calculez le temps d'exécution
+execution_time = end_time - start_time  # Calculez le temps d'exécution
 
 # Afficher les résultats
 print("Prix total :", total_price)
